853a84d837f9a249ae945b64507653ef780a1226
YATHARTH <TYAGIYATHARTH31@GMAIL.COM>
2022-06-13 01:11:24 +0530

    13/06

--- js/index.js
+++ js/index.js
@@ -5,16 +5,33 @@ let moveSound = new Audio ('move.mp3');
 let musicSound = new Audio('music.mp3');
 let speed =2;
 let lastPaintTime =0;
+let snakeArr = [
+    {x:13 , y: 15}
+]
 
 
 //game functions
 function main(ctime){
 
     window.requestAnimationFrame(main);
-    if((ctime - lastPaintTime)/1000 < 1/speed);
     console.log(ctime)
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+  
 }
 
 function gameEngine(){
-    
+    board  .innerHTML = "";
+    snakeArr.forEach((e, index) => {
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        snakeElement.classList.add('food')
+        board.appendChild(snakeElement);
+        
+        
+    })
 }
